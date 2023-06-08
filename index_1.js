82b4ab6407dbbe72235a8ec557b85e544be2370e
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-06-08 17:19:12 +0600

    For of loop

--- index_1.js
+++ index_1.js
@@ -14,3 +14,8 @@ const person= {
 for (let key in person){
     console.log(key,person[key]);
 }
+//for of loop
+const colors = ["red","green","blue"];
+for(let color of colors){
+    console.log(color);
+}
