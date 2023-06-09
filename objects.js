3e72ae57348647a0c87fc3c7e50ef03d48deb76c
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-06-09 15:46:22 +0600

    Basic of OOP

---
+++ objects.js
@@ -0,0 +1,14 @@
+//Basic Object_oriented Programing(OOP)
+ const circle = {
+   radious: 2,
+   laocation: {
+    x: 1,
+    y: 1
+   },
+   isVisible: true,
+   draw: function() {
+    console.log("Draw");
+   }
+ };
+
+ circle.draw();
