b313e9a6883039325d1c3e8f70f0e9ffce7df799
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-06-21 23:08:05 +0600

    bunch of stuff  about functions

---
+++ functionDeclarevsExpression.js
@@ -0,0 +1,17 @@
+// function declaration
+function walk(){
+    console.log('walk');
+}
+
+// function expression
+
+let run = function reu(){
+    console.log('run');
+};
+
+//anonymous function expression
+
+let pace = function(){
+    console.log('pace');
+};
+pace();
