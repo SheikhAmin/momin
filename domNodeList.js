43d7ee17cf69c1472625878e8e40b7496248dea2
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2023-07-01 20:13:51 +0600

    dom basics done

---
+++ domNodeList.js
@@ -0,0 +1,10 @@
+const nodeList = document.querySelectorAll('.Hello');
+const htmlCollection = document.getElementsByClassName('World');
+
+
+console.dir(nodeList);
+console.dir(htmlCollection);
+
+
+console.log(nodeList[0]);
+console.log(htmlCollection[0]);
